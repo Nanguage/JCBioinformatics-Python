546a44decf3370a31b86be4f3a54d9a25a650c2d
nanguage <nanguage@yahoo.com>
2019-07-16 22:50:35 +0800

    update

--- make_pages.sh
+++ make_pages.sh
@@ -8,7 +8,8 @@ do
     cd ${page_dir}
     for ipnb in $(ls | grep .ipynb); do
         echo ${ipnb}
-        jupyter nbconvert ${ipnb} --to slides --SlidesExporter.reveal_scroll=True --SlidesExporter.reveal_transition=none
+        jupyter nbconvert ${ipnb} --to slides --SlidesExporter.reveal_scroll=True --SlidesExporter.reveal_transition=none --stdout > ./slides_${ipnb}.html
+        jupyter nbconvert ${ipnb} --to html --stdout > ./page_${ipnb}.html
     done
     mv ./*.html ../docs
     if [ -d ./img ];then
