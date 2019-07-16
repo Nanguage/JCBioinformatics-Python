9f17303793bbc40ccebd0c00028a19d59539a251
nanguage <nanguage@yahoo.com>
2019-07-16 21:07:13 +0800

    update

---
+++ make_pages.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+IFS=$'\n'
+
+for page_dir in $(ls | grep 'part');
+do
+    cd ${page_dir}
+    for ipnb in $(ls | grep .ipynb); do
+        echo ${ipnb}
+        jupyter nbconvert ${ipnb} --to slides --SlidesExporter.reveal_scroll=True --SlidesExporter.reveal_transition=none
+    done
+    mv ./*.html ../docs
+    if [ -d ./img ];then
+        cp ./img/* ../docs/img
+    fi
+    cd ..
+done
