fa2460cced2cee7ec8416ae99e235c1d016045f7
nanguage <nanguage@yahoo.com>
2019-07-19 01:56:58 +0800

    update

--- make_pages.sh
+++ make_pages.sh
@@ -3,13 +3,19 @@
 set -e
 IFS=$'\n'
 
+if [ -x "$(command -v jupyter)" ];then
+    JUPYTER="jupyter"
+else
+    JUPYTER="jupyter.exe"  # use in WSL environment
+fi
+
 for page_dir in $(ls | grep 'part');
 do
     cd ${page_dir}
     for ipnb in $(ls | grep .ipynb); do
         echo ${ipnb}
-        jupyter nbconvert ${ipnb} --to slides --SlidesExporter.reveal_scroll=True --SlidesExporter.reveal_transition=none --stdout > ./slides_${ipnb}.html
-        jupyter nbconvert ${ipnb} --to html --stdout > ./page_${ipnb}.html
+        ${JUPYTER} nbconvert ${ipnb} --to slides --SlidesExporter.reveal_scroll=True --SlidesExporter.reveal_transition=none --stdout > ./slides_${ipnb}.html
+        ${JUPYTER} nbconvert ${ipnb} --to html --stdout > ./page_${ipnb}.html
     done
     mv *.html ../docs
     if [ -d ./img ];then
